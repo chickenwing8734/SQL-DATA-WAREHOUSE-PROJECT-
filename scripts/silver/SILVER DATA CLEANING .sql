67e6fc1fc16355091d70d466bd2f6a686ee523a4
chickenwing8734 <153263801+chickenwing8734@users.noreply.github.com>
2026-02-06 02:39:43 +0800

    Update SILVER DATA CLEANING .sql

--- scripts/silver/SILVER DATA CLEANING .sql
+++ scripts/silver/SILVER DATA CLEANING .sql
@@ -1,21 +1,35 @@
 -- Clearing dublicates
-SELECT *
-FROM (
-SELECT 
-*,
-ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_CREATE_date DESC) as flag_last
-FROM bronze.crm_cust_info
-)t WHERE flag_last= 1
+INSERT INTO silver.crm_cust_info (
+	cst_id, 
+	cst_key, 
+	cst_firstname, 
+	cst_lastname, 
+	cst_marital_status, 
+	cst_gndr,
+	cst_create_date
+)
 
 --Checking for unwanted Spaces
 SELECT 
 cst_id,
+cst_key,
 TRIM(cst_firstname) AS cst_firstname,
 TRIM(cst_lastname) AS cst_lastname,
-cst_material_status,
+CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'SINGLE'
+	WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'MARRIED'
+	ELSE 'N/A'
+END cst_material_status,
 CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'FEMALE'
 	WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'MALE'
 	ELSE 'N/A'
 END cst_gndr,
 cst_create_date
+-- Clearing dublicates
+FROM (
+SELECT 
+*,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_CREATE_date DESC) as flag_last
 FROM bronze.crm_cust_info
+WHERE cst_id IS NOT NULL
+)t 
+WHERE flag_last= 1;
